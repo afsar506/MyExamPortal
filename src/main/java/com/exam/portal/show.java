fe9cc83950b57e2ca491e39086b48873053437f7
afsar <afsarhussain506@gmai.com>
2026-07-07 02:51:39 +0530

    first commit

---
+++ src/main/java/com/exam/portal/show.java
@@ -0,0 +1,13 @@
+package com.exam.portal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class show {
+   @GetMapping("/show")
+    public String message()
+    {
+        return "Hello There, This is afsar khan ";
+    }
+}
